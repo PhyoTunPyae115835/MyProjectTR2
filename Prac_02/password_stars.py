17f5794a6e3294443582c3df74aeb8a2236b803e
Pyae Phyo Tun <phyotun.pyae@my.jcu.edu.au>
2025-06-01 18:29:06 +0800

    Add basic password check script
    Extract get_password() function from main
    
    Extract print_asterisks() function

--- Prac_02/password_stars.py
+++ Prac_02/password_stars.py
@@ -12,5 +12,9 @@ def get_password():
     """Prompt the user to enter a password and return it."""
     return input("Enter password: ")
 
+def print_asterisks(password):
+    """Print asterisks equal to the length of the password."""
+    print('*' * len(password))
+
 
 main()
