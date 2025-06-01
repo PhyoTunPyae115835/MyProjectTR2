d17e565cb682d27f499d11f894326fd67b5a5b20
Pyae Phyo Tun <phyotun.pyae@my.jcu.edu.au>
2025-06-01 18:27:57 +0800

    Add basic password check script
    Extract get_password() function from main

--- Prac_02/password_stars.py
+++ Prac_02/password_stars.py
@@ -1,2 +1,16 @@
-password = input("Enter password: ")
-print('*' * len(password))
+"""
+Program to prompt the user for a password and display asterisks
+matching the length of the password.
+"""
+
+def main():
+    password = get_password()
+    print('*' * len(password))
+
+
+def get_password():
+    """Prompt the user to enter a password and return it."""
+    return input("Enter password: ")
+
+
+main()
