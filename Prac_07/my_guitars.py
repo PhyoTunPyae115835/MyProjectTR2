ae8162b448b4c757c6811f9e29a1ea7f7b8ed6c0
Pyae Phyo Tun <phyotun.pyae@my.jcu.edu.au>
2025-07-13 20:59:51 +0800

    Week 7
    # Copy guitar class
    
    guitar.py:
    - Add sorting by year with __lt__ method in Guitar class
    
    my_guitars.py:
    - Load and display guitars from CSV in my_guitars.py
    - Update main function

--- Prac_07/my_guitars.py
+++ Prac_07/my_guitars.py
@@ -5,6 +5,15 @@ Guitar program to load, display, and manage guitars.
 from guitar import Guitar
 import csv
 
+def main():
+    """Main function to load, sort, and display guitars."""
+    filename = "guitars.csv"
+    guitars = load_guitars(filename)
+
+    # Sort guitars by year
+    guitars.sort()
+
+    display_guitars(guitars)
 
 def load_guitars(filename):
     """Load guitars from a CSV file and return a list of Guitar objects."""
@@ -22,14 +31,6 @@ def display_guitars(guitars):
     print("These are the current guitars:")
     for i, guitar in enumerate(guitars, 1):
         print(f"Guitar {i}: {guitar}")
-
-
-def main():
-    """Main function to load and display guitars."""
-    filename = "guitars.csv"
-    guitars = load_guitars(filename)
-    display_guitars(guitars)
-
 
 if __name__ == "__main__":
     main()
