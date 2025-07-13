e58f309f08ee72a1270c4b1dd04018c23fe15dfc
Pyae Phyo Tun <phyotun.pyae@my.jcu.edu.au>
2025-07-13 20:44:24 +0800

    Week 6
    guitar.py:
    - Add Guitar class with __init__, __str__, get_age, and is_vintage methods
    - Minor change
    
    guitar_test.py:
    - Add guitar_test.py to manually test get_age() and is_vintage() methods
    
    # Add guitars.csv
    
    my_guitars.py:
    - Add loading and displaying guitars from CSV in my_guitars.py
    - Add user input loop for adding new guitars
    - Add helper functions to validate integer and float input
    - Add sorting and formatted display of guitars with vintage label

--- Prac_06/my_guitars.py
+++ Prac_06/my_guitars.py
@@ -23,6 +23,12 @@ def main():
         print(f"{new_guitar} added.\n")
         name = input("Name: ")
 
+        guitars.sort()
+        print("\nThese are my guitars:")
+        for i, guitar in enumerate(guitars, 1):
+            vintage_string = " (vintage)" if guitar.is_vintage() else ""
+            print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
 def load_guitars(filename):
     """Load guitars from a CSV file and return a list of Guitar objects."""
     guitars = []
