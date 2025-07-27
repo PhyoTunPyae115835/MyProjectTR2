40a1184f7b279a246cc8276823dc6892ae92a81e
Pyae Phyo Tun <phyotun.pyae@my.jcu.edu.au>
2025-07-27 16:55:38 +0800

    - Add test file for silver_service_taxi_test.py with expected output of $48.80

---
+++ Prac_09/silver_service_taxi_test.py
@@ -0,0 +1,9 @@
+from Prac_09.silver_service_taxi import SilverServiceTaxi
+
+def main():
+    fancy_taxi = SilverServiceTaxi("Hummer", 200, 2)
+    fancy_taxi.drive(18)
+    print(fancy_taxi)
+    print(f"Total fare: ${fancy_taxi.get_fare():.2f}")
+
+main()
